97f8d2b204557d0ad14642057e5997e19177b6e9
xccd <64030555+ionxccd@users.noreply.github.com>
2022-09-17 22:33:50 -0400

    Update LangaugePage.vue

--- src/components/LangaugePage.vue
+++ src/components/LangaugePage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class='row justify-content-around w-200'>
       <div class="col-3">
-        <img src="../src/images/nodejs.png" width=auto height="100" id="marginauto"/>
+        <img src="../images/nodejs.png" width=auto height="100" id="marginauto"/>
       </div>
       <div class="col-3">
         <img src="../images/python.png" width=auto height="100" id="marginauto"/>
